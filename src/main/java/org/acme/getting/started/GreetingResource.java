119a811085b144cdba14de550db75192cfaabb8d
Prashu <prashusaxena23@gmail.com>
2025-02-13 21:18:30 +0530

    test

--- src/main/java/org/acme/getting/started/GreetingResource.java
+++ src/main/java/org/acme/getting/started/GreetingResource.java
@@ -16,6 +16,7 @@ public class GreetingResource {
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/greeting/{name}")
     public String greeting(String name) {
+    	
         return service.greeting(name);
     }
 
